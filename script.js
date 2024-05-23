35e493c865fe16cd1009b1512d8da2f45003492e
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-23 21:51:36 +0530

    init: complete code

---
+++ script.js
@@ -0,0 +1,32 @@
+let userDetail = {
+    userName: "Lubna",
+    isWorking: true,
+    location: "Chennai",
+    learning: "JS",
+}
+
+userDetail["favourite dish"] = "Biryani";
+
+// console.log(userDetail);
+
+
+let newUser = {};
+let key = "name";
+let value = "Jane";
+
+// newUser = {
+//     key: value,
+// }
+
+// newUser.key = value;
+// newUser[key] = newUser[value];
+newUser[key] = value;
+
+console.log(newUser);
+// console.log(newUser.key = value);
+// console.log(newUser[key] = value);
+
+
+// newUser =  {
+//    name: "Jane",
+// }
